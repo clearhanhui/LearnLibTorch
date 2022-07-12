8fbf70a2c3633ae342d4a1cb058c99e662f87072
hanhui <193691140@qq.com>
2022-07-13 00:55:02 +0800

    * lenet5 mnist * fix typo

---
+++ chap5/python/Lenet5MNIST.py
@@ -0,0 +1,71 @@
+from sklearn.model_selection import cross_val_predict
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+
+
+class LeNet5(nn.Module):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, (5, 5), padding=2)
+        self.conv2 = nn.Conv2d(6, 16, (5, 5))
+        self.fc1 = torch.nn.Linear(16*5*5, 120)
+        self.fc2 = torch.nn.Linear(120, 84)
+        self.fc3 = torch.nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.reshape((x.size(0), -1))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+train_dataset = torchvision.datasets.MNIST(root='../data/',
+                                           train=True,
+                                           transform=transforms.ToTensor(),
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='../data/',
+                                          train=False,
+                                          transform=transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=128,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=128,
+                                          shuffle=False)
+
+model = LeNet5()
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+for i in range(5):
+    for x, y in train_loader:
+        y_prob = model(x)
+        loss = loss_fn(y_prob, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    print("Epoch [{:0>2d}]  loss={:.4f}".format(i, loss.item()))
+
+# torch.save(model, 'lenet5.pt')
+# model = torch.load('lenet5.pt')
+# model.eval()
+
+correct = 0
+total = 0
+for x, y in test_loader:
+    y_prob = model(x)
+    _, y_ = torch.max(y_prob, 1)
+    correct += (y_ == y).sum().item()
+    total += y.size(0)
+
+print('Test Accuracy = {:.2f} %'.format(100 * correct / total))
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@
 #  LibTorch 教程 
 
 ## 简介
-网上关于 LibTorch 的中文教程太少了，最近也要用到就好好学习总结下。
+网上关于 LibTorch 的中文教程和资料太少了，最近也要用到就好好学习总结下。
 
 每个目录是独立一个 [CMake](https://cmake.org/) 项目，项目的主要参考内容是 [PyTorch 官方文档](https://pytorch.org/docs/stable/index.html)和网上的一些中英文博客资料。
 
@@ -17,18 +17,12 @@
 * [1--LibTorch HelloWorld](./chap1/)
 * [2--张量基础](./chap2/)
 * [3--自动微分](./chap3/)
-* [4--基本模型](./chap4/build/CMakeFiles/3.23.2/CompilerIdC/CMakeCCompilerId.c)
+* [4--基本模型](./chap4/)
 
 
 ## 软件环境
 
-* libtorch
-* cmake 
-* gcc 
-* make
-* python
-
-详细的环境配置可以参考[这里](./chap0/)
+详细的环境配置可以参考[这里](./chap0/)。
 
 
 ----------------
